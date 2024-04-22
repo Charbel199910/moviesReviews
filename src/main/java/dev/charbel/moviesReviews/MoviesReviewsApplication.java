d86f486c0297a39813f3be3dd8c9e5dcbab9c9df
charbelbaaklini <baaklinicharbel3@gmail.com>
2024-04-21 21:09:26 -0400

    Initial commit: Added backend code

--- src/main/java/dev/charbel/moviesReviews/MoviesReviewsApplication.java
+++ src/main/java/dev/charbel/moviesReviews/MoviesReviewsApplication.java
@@ -2,8 +2,10 @@ package dev.charbel.moviesReviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 
 public class MoviesReviewsApplication {
 
